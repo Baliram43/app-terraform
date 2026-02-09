7baa99e9719bde024c29c0f9502d540d93631676
Baliram43 <136733451+Baliram43@users.noreply.github.com>
2026-02-09 09:51:05 +0530

    commented

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_vpc" "awsvpc" {
     Name = var.net_info.vpcname
   }
 }
+#all 4-subnets in one resources block
 resource "aws_subnet" "subnets" {
   count             = length(var.net_info.subnets_info[0].subaz) #4=hardcoded
   vpc_id            = aws_vpc.awsvpc.id
@@ -19,23 +20,7 @@ resource "aws_subnet" "subnets" {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* i want 4-subnets converted into 1-subnet like this in a single subnet source but not hardcoded, so we written above in terms of varibales
+/* i want 4-subnets converted into 1-subnet resource block but not hardcoded, so we written above in terms of varibales
 resource "aws_subnet" "websubnet" {
   vpc_id            = aws_vpc.awsvpc.id
   cidr_block        = ["10.0.0./24","10.1.0./24","10.2.0./24","10.3.0./24"]
